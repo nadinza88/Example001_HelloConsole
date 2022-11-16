bac9551de2f800b100b415c2a3d0d25564121e1d
nadinza88 <nadimihalenko@gmail.com>
2022-11-16 14:50:00 +0300

    add sem001

---
+++ sem01_01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+
+string input = Console.ReadLine (); // Ввод 
+
+int number = int.Parse (input); 
+
+int square = number * number; 
+
+Console.WriteLine ($"Число в квадрате равно {square}");
+
